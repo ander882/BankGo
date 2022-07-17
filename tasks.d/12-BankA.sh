9d129f2f9e198174f34af801910c609cfc650191
Craig J. anderson <ander882@hotmail.com>
2022-07-17 10:38:02 -0400

    Update 12-BankA.sh

--- tasks.d/12-BankA.sh
+++ tasks.d/12-BankA.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-
 taskString="12 Bank Home"
-
 startTask "$taskString"
+
 
 # We are already being directed here!  
 # No need to go anywhere.  Just wait for it to load
 
-masterpid=$(xdotool getwindowfocus)
 
-
+#Check again that Chrome is the active window
 winname=$(xdotool getwindowname $(xdotool getwindowfocus))
 if [[ "$winname" != *"Chromium"* ]]; then
   log "No web browser opened"
@@ -18,8 +16,11 @@ if [[ "$winname" != *"Chromium"* ]]; then
   exit
 fi
 
-#New Window and get login page
 
+# Use the 'select all' and 'copy' technique to see
+# when the page has loaded enough.  Some elements are 'active'
+# so they load after the page has loaded.  
+# Here is the string that we are going to be looking for.
 checkstring="Checking (Checking)"
 
 for i in {1..6}
@@ -38,10 +39,15 @@ do
 
 done
 
+# echo will sometime throw an error.  Why?  inproperlly formatted html?
+# printf works well with this bank.  So use it.
+# printing the scraped web page will save to a file from the script that called this.
 #echo "$scrape"
 printf "$scrape"
 #log "$scrape"
 
+
+# Stop this bank process if we didn't find what we wanted.
 if [[ "$scrape" != *"$checkstring"* ]]; then
   log "Scrape not correct"
   stopProcess
