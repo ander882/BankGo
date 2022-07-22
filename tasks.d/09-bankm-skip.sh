cd0960f418ea3cf151e7f3e236e6b01ac025e769
Craig J. anderson <ander882@hotmail.com>
2022-07-22 14:48:57 -0400

    Update 09-bankm-skip.sh

--- tasks.d/09-bankm-skip.sh
+++ tasks.d/09-bankm-skip.sh
@@ -12,6 +12,10 @@ oldnum=`cut -f1 ${CONFIG[dbDir]}/bankm-skip.txt`
 newnum=`expr $oldnum + 1`
 printf "%04d" $newnum > ${CONFIG[dbDir]}/bankm-skip.txt
 
+# (( newnum % 3 )) will give:
+# 0 = false
+# 1 = true
+# 2 = true
 
 if (( newnum % 3 )); then
 	log "skipping ${VALUE} this time"
