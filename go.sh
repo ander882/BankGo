92b4b6639eb6ce14adf5073016435b47f9e1e955
Craig J. anderson <ander882@hotmail.com>
2022-09-01 13:33:24 -0400

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# My main 'do it all' file.  just a ./go.sh and it's off.
+
+# -h . 
+# # The home directory is here
+# -c
+# # Now that I have 2 programs, run this one only.
+# # bashmanager defaults to all
+# -v
+# # Verbose.  I like to see the messages roll on by.
+
 bashman -h . -c bankgo -v
 #bashman -h . 
 
